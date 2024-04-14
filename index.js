8e29c13ee5446ac8d9d26e24825c1dd622564f00
Jamie Buick <jamiebuick28@gmail.com>
2024-04-14 13:51:19 +0100

    Have gotten the POST working, so the user can now send data from the form to the server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import { name } from "ejs";
 import express from "express";
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/", (req, res)=> {
@@ -17,7 +21,33 @@ app.get("/", (req, res)=> {
     res.render("write.ejs")
   });
   
+
+  app.post("/submit", (req, res) => {
+    console.log('Received form submission:');
+    console.log('Request body:', req.body);
+
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const title = req.body.title;
+    const content = req.body.content;
+
+       // Here, you can handle the form data as you wish, such as saving it to a database or logging it
+       console.log('Submitted form data:');
+       console.log('Name:', name);
+       console.log('Email:', email);
+       console.log('Title:', title);
+       console.log('Content:', content);
+      
+      
+    
+  });
   
+  
+
+
+
+
  
   
   app.listen(port, () => {
