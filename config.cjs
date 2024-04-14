c99597a66e3fe2e1b9e493ffb6e25e744a895ea5
Jamie Buick <jamiebuick28@gmail.com>
2024-04-14 19:58:03 +0100

    SQL database connected, running on my local pc, this is so that I can keep history of the entries of the blog site and make it actually useable

---
+++ config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    server: 'JAMIE-HOME\\SQLEXPRESS',
+    database: 'blogger',
+    user: 'jbuick',
+    password: 'J2a8m0i5e!!',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { name } from "ejs";
 import express from "express";
+import sql from "mssql";
+import config from './config.cjs';
 
 const app = express();
 const port = 3000;
@@ -8,40 +10,67 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+let submitted = false;
+
+
+
+
+// Connect to the database
+sql.connect(config)
+  .then(pool => {
+    console.log('Connected to SQL Server database');
+    // Query data or perform other database operations here
+  })
+  .catch(err => {
+    console.error('Error connecting to SQL Server database:', err);
+});
+
+
+
+
+
+
 app.get("/", (req, res)=> {
   res.render("index.ejs")
-  });
+});
   
-
+  
 /*   app.get("/blogs", (req, res)=> {
     res.render("blogs.ejs")
   }); */
 
-  app.get("/write", (req, res)=> {
-    res.render("write.ejs")
-  });
-  
+app.get("/write", (req, res)=> {
+  res.render("write.ejs")
+});
+
 
   app.post("/submit", (req, res) => {
-    console.log('Received form submission:');
-    console.log('Request body:', req.body);
-
-
-    const name = req.body.name;
-    const email = req.body.email;
-    const title = req.body.title;
-    const content = req.body.content;
-
-       // Here, you can handle the form data as you wish, such as saving it to a database or logging it
-       console.log('Submitted form data:');
-       console.log('Name:', name);
-       console.log('Email:', email);
-       console.log('Title:', title);
-       console.log('Content:', content);
-      
-      
     
-  });
+    const today = new Date();
+    submitted = true;
+
+    const post = ({
+       name: req.body.name,
+       email:  req.body.email,
+       title: req.body.title,
+       content: req.body.content,
+       date : today
+    });
+
+
+   
+
+    // Here, you can handle the form data as you wish, such as saving it to a database or logging it
+    console.log('Submitted form data:');
+    console.log('Post:', post);
+
+    console.log('Submitted:', submitted);
+    console.log('today:', today);
+
+    // Redirect or render a thank you page
+    res.render("index.ejs", {locals: post});
+  
+});
   
   
 
